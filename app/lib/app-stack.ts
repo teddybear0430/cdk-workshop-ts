f6856561c846446913d9c63091776a06bb3e2845
teddybear0430 <teddybear0430@hotmail.com>
2022-12-11 16:51:03 +0900

    style: Format of code.

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,8 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    // defines an AWS Lambda resource
   }
 }
