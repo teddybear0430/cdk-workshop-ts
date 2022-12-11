a983074fe9177982410dd25abe77d7deca91043c
teddybear0430 <teddybear0430@hotmail.com>
2022-12-11 21:55:34 +0900

    feat: Defines an api gateway.

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,10 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
     // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      // execution environment
+      runtime: lambda.Runtime.NODEJS_16_X,
+      // code loaded from "lambda" directory
+      code: lambda.Code.fromAsset('lambda'),
+      // file is "hello", function is "handler"
+      handler: 'hello.handler',
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+    });
   }
 }
